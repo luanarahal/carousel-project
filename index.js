062519bc86566d55dbc8ec2c5c5f4732c08dba2f
Adriano Prates <adrianopratesr@gmail.com>
2024-02-27 01:58:01 -0300

    feat: add logic banner

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const API_CATS_URL = "https://api.thecatapi.com/v1/images/search?limit=10";
+const previousButtonHTML = document.getElementById("previousButton");
+const nextButtonHTML = document.getElementById("nextButton");
+const imageBannerHTML = document.getElementById("imageBanner");
+let allUrlsImages = [];
+let contador = 0;
+
+window.addEventListener("load", () => {
+  imageBannerHTML();
+});
 
 const listUrlsImages = (responseData) => {
   const urlsImages = [];
@@ -15,4 +24,38 @@ const listTenImagesUrls = async () => {
   return listUrls;
 };
 
-listTenImagesUrls();
+const main = async () => {
+  const list = await listTenImagesUrls();
+  imageBannerHTML.setAttribute("src", list[contador]);
+  allUrlsImages = list;
+  console.log(allUrlsImages);
+};
+
+main();
+
+nextButtonHTML.addEventListener("click", () => {
+  if (contador == allUrlsImages.length - 1) {
+    contador = 0;
+  } else {
+    contador++;
+  }
+  imageBannerHTML.setAttribute("src", allUrlsImages[contador]);
+});
+
+previousButtonHTML.addEventListener("click", () => {
+  if (contador == 0) {
+    contador = allUrlsImages.length - 1;
+  } else {
+    contador--;
+  }
+  imageBannerHTML.setAttribute("src", allUrlsImages[contador]);
+});
+
+setTimeout(() => {
+  if (contador == allUrlsImages.length - 1) {
+    contador = 0;
+  } else {
+    contador++;
+  }
+  imageBannerHTML.setAttribute("src", allUrlsImages[contador]);
+}, 5000);
